8280dad14f4c894b744a3b9cb498a90119057659
SunCloud1410 <97592251+SunCloud1410@users.noreply.github.com>
2023-05-27 00:05:15 +0200

    Added more test on item
    After reflexion added more test to cover more use cases

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -7,6 +7,7 @@ describe('Gilded Rose', () => {
         new Item('Normal Item 1', 5, 10),
         new Item('Normal Item 2', 0, 10),
         new Item('Normal Item 3', 5, 0),
+        new Item('Normal Item 4', -4, 10),
       ];
       const gildedRose = new GildedRose(items);
       const updatedItems = gildedRose.updateQuality();
@@ -17,11 +18,15 @@ describe('Gilded Rose', () => {
       
       expect(updatedItems[1].name).toBe('Normal Item 2');
       expect(updatedItems[1].sellIn).toBe(-1);
-      expect(updatedItems[1].quality).toBe(8);
+      expect(updatedItems[1].quality).toBe(9);
       
       expect(updatedItems[2].name).toBe('Normal Item 3');
       expect(updatedItems[2].sellIn).toBe(4);
       expect(updatedItems[2].quality).toBe(0);
+
+      expect(updatedItems[3].name).toBe('Normal Item 4');
+      expect(updatedItems[3].sellIn).toBe(-5);
+      expect(updatedItems[3].quality).toBe(8);
     });
     
     it('should handle Aged Brie', () => {
@@ -35,13 +40,28 @@ describe('Gilded Rose', () => {
     });
     
     it('should handle Backstage passes', () => {
-      const item = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10);
-      const gildedRose = new GildedRose([item]);
+      const item = [new Item('Backstage passes to a TAFKAL80ETC concert', 11, 10),
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 6, 10),
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 4, 10),
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
+      const gildedRose = new GildedRose(item);
       const updatedItems = gildedRose.updateQuality();
       
       expect(updatedItems[0].name).toBe('Backstage passes to a TAFKAL80ETC concert');
       expect(updatedItems[0].sellIn).toBe(10);
       expect(updatedItems[0].quality).toBe(11);
+
+      expect(updatedItems[1].name).toBe('Backstage passes to a TAFKAL80ETC concert');
+      expect(updatedItems[1].sellIn).toBe(5);
+      expect(updatedItems[1].quality).toBe(12);
+
+      expect(updatedItems[2].name).toBe('Backstage passes to a TAFKAL80ETC concert');
+      expect(updatedItems[2].sellIn).toBe(3);
+      expect(updatedItems[2].quality).toBe(13);
+
+      expect(updatedItems[3].name).toBe('Backstage passes to a TAFKAL80ETC concert');
+      expect(updatedItems[3].sellIn).toBe(-1);
+      expect(updatedItems[3].quality).toBe(0);
     });
     
     it('should handle Sulfuras', () => {
@@ -55,11 +75,11 @@ describe('Gilded Rose', () => {
     });
     
     it('should handle Conjured items', () => {
-      const item = new Item('Conjured Item', 5, 10);
+      const item = new Item('Conjured Generic Item Name', 5, 10);
       const gildedRose = new GildedRose([item]);
       const updatedItems = gildedRose.updateQuality();
       
-      expect(updatedItems[0].name).toBe('Conjured Item');
+      expect(updatedItems[0].name).toBe('Conjured Generic Item Name');
       expect(updatedItems[0].sellIn).toBe(4);
       expect(updatedItems[0].quality).toBe(8);
     });
