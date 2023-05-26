489f843e80592b3a6c4471b1b1c758a503740f7c
SunCloud1410 <97592251+SunCloud1410@users.noreply.github.com>
2023-05-27 00:06:16 +0200

    UpdateQuality method refactor
    Main work of the project.
    Updated the updateQuality method to fit the requirement

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -17,53 +17,87 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
+  updateQuality(): Item[] {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
+      const item = this.items[i];
+
+      // Update quality based on item name
+      switch (item.name) {
+        case 'Aged Brie':
+          this.updateAgedBrie(item);
+          break;
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          this.updateBackstagePasses(item);
+          break;
+        case 'Sulfuras, Hand of Ragnaros':
+          // Legendary item, no need to update
+          break;
+        default:
+          if (item.name.startsWith('Conjured')) {
+            this.updateConjured(item);
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.updateNormalItem(item);
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+      }
+
+      // Decrease sellIn for all items except Sulfuras
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn--;
+      }
+
+      // Ensure quality stays within the valid range
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.quality = Math.min(50, Math.max(0, item.quality));
+      }
+
+    }
+    return this.items;
+  }
+
+  private updateNormalItem(item: Item): void {
+  
+    if (item.quality > 0) {
+      item.quality--;
+    }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality--; // Quality degrades twice as fast after expiration
+    }
+    if (item.name === 'Normal Item +10') {
+      console.log("sellIn after update",item.sellIn)
+      console.log("quality after update",item.quality)
+    }
+  }
+
+  private updateAgedBrie(item: Item): void {
+    if (item.quality < 50) {
+      item.quality++;
+    }
+    if (item.sellIn < 0 && item.quality < 50) {
+      item.quality++; // Quality increases even after expiration
+    }
+  }
+
+  private updateBackstagePasses(item: Item): void {
+    if (item.quality < 50 && item.sellIn > 0) {
+      item.quality++;
+      if (item.sellIn <= 10 && item.quality < 50) {
+        item.quality++; // Quality increases by 2 when there are 10 days or less
+      }
+      if (item.sellIn <= 5 && item.quality < 50) {
+        item.quality++; // Quality increases by 3 when there are 5 days or less
       }
     }
+    else {
+      item.quality = 0; // Quality drops to 0 after the concert
+    }
+  }
 
-    return this.items;
+  private updateConjured(item: Item): void {
+    if (item.quality > 0) {
+      item.quality -= 2;
+    }
+    if (item.sellIn < 0 && item.quality > 0) {
+      item.quality -= 2; // Quality degrades twice as fast after expiration
+    }
   }
 }
